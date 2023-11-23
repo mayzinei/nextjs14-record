18f1bd1a7927a8d11636909de2ee394c6a551d18
mayzinei <mayzinei1998@gmail.com>
2023-11-23 14:21:11 +0630

    fix private folder

--- app/_lib/page.tsx
+++ app/_lib/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function PrivateRoute() {
-	return <div>PrivateRoute</div>;
+	return (
+		<div>
+			<h1>You can not view this page in the browser</h1>
+		</div>
+	);
 }
