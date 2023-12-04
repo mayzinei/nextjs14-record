ce602b4214a83e00a2574c1b006f83be797573e6
mayzinei <mayzinei1998@gmail.com>
2023-12-04 22:54:49 +0630

    link component navigation

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,12 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Items() {
+	const productId = 100;
 	return (
-		<div>
+		<div className="flex flex-col gap-2 p-8">
+			<Link href="/" className="font-bold">
+				Home
+			</Link>
 			<h1>Item Lists</h1>
-			<p>Item 1</p>
-			<p>Item 2</p>
-			<p>Item 3</p>
+			<h2>
+				<Link href="items/1">Item 1</Link>
+			</h2>
+			<h2>
+				<Link href="items/2">Item 2</Link>
+			</h2>
+			{/* "Replace" can go Back to home page */}
+			<h2>
+				<Link href="items/2">Item 2</Link>
+			</h2>
+			<h2>
+				<Link href={`items/${productId}`}>Product {productId}</Link>
+			</h2>
 		</div>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
 	return (
-		<div>
+		<div className="flex flex-col gap-4">
 			<h1>Hello World</h1>
+			{/* NextJs Navigation is file base routing */}
+
+			<div className="p-6 flex flex-col gap-3 font-bold">
+				<Link href="/about">About</Link>
+				<Link href="/items">Items</Link>
+			</div>
 		</div>
 	);
 }
