8b9286922c22afeeaed55bd934512a60f500f1a7
mayzinei <mayzinei1998@gmail.com>
2023-12-23 12:51:35 +0630

    loading page

---
+++ app/products/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Loading() {
+	return (
+		<div>
+			<h1>Loading ...</h1>
+		</div>
+	);
+}
+
+// when the page contents are loading, show loading page
