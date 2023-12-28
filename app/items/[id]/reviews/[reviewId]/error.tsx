a8d89287ea43a124ed4e3c135721ae7f44c249c2
mayzinei <mayzinei1998@gmail.com>
2023-12-28 10:27:41 +0630

    recover error

--- app/items/[id]/reviews/[reviewId]/error.tsx
+++ app/items/[id]/reviews/[reviewId]/error.tsx
@@ -2,10 +2,19 @@
 
 import React from "react";
 
-export default function ErrorBoundry({ error }: { error: Error }) {
+export default function ErrorBoundry({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
 	return (
 		<div>
 			<h1>{error.message}</h1>
+			<button onClick={reset} className="p-2 bg-slate-400 rounded-md">
+				Try Again
+			</button>
 		</div>
 	);
 }
